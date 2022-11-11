a0d7d3c522a7357c33595f5873176382512cc524
rafa <homer512x@gmail.com>
2022-11-11 17:05:07 +0100

    incluido el ejercicio en main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+//Crea un array de 3 pokemons.
+
+var pokemon = ["Bulbasaur", "Ivysaur", "Venusaur"]
+
+//Muestra por consola el nombre de cada pokemon.
+
+console.log(pokemon)
+
+//Bonus de investigación: ¿Cómo se puede mostrar el nombre de cada pokemon en una línea?
+
+console.log(pokemon[0])
+console.log(pokemon[1])
+console.log(pokemon[2])
+
+//Bonus extra: ¿Cómo se puede hacer esto con bucles?.
+
+for (var i = 0; i < pokemon.length; i++) {
+	console.log(pokemon[i])
+}
